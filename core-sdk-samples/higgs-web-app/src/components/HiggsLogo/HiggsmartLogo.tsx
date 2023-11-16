4a1539d7ab574c3dbdaf86dcf6afc1b6bd097aa8
rpickettmp <87021361+rpickettmp@users.noreply.github.com>
2023-11-16 15:21:55 +0000

    Update HiggsmartLogo.tsx

--- core-sdk-samples/higgs-web-app/src/components/HiggsLogo/HiggsmartLogo.tsx
+++ core-sdk-samples/higgs-web-app/src/components/HiggsLogo/HiggsmartLogo.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@mui/material';
 // import logo from '../../assets/images/higgsmart-logo.svg';
-import logo from 'https://www.monsoon.co.uk/on/demandware.static/-/Library-Sites-monsoon-content-global/default/dwe907f9ba/global/monsoon-logo-white-3.png';
+import logo from '../../assets/images/monsoon-logo-white-3.png';
 
 const HiggsmartLogo = () => (
     <Box
