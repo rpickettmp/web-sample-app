56d9941a146d8b6bf537a78bdb0900b8a4f5050f
rpickettmp <87021361+rpickettmp@users.noreply.github.com>
2023-11-17 09:22:22 +0000

    Update Products.ts

--- core-sdk-samples/higgs-web-app/src/models/Products.ts
+++ core-sdk-samples/higgs-web-app/src/models/Products.ts
@@ -23,9 +23,9 @@ const products: Product[] = [
         id: '64170109',
         label: 'DI DOUBLE BOW JUMPER RED',
         imageUrl:
-            'https://www.monsoon.co.uk/dw/image/v2/BDLV_PRD/on/demandware.static/-/Sites-monsoon-master-catalog/default/dwdf676ffa/images/large/51_64170109_6.jpg',
+            'https://www.monsoon.co.uk/dw/image/v2/BDLV_PRD/on/demandware.static/-/Sites-monsoon-master-catalog/default/dwdf676ffa/images/large/51_64170109_6.jpg?sw=663&sh=848&sm=cut',
         imageUrl2:
-            'https://www.monsoon.co.uk/dw/image/v2/BDLV_PRD/on/demandware.static/-/Sites-monsoon-master-catalog/default/dw23cff41c/images/large/23_64170109_2.jpg',
+            'https://www.monsoon.co.uk/dw/image/v2/BDLV_PRD/on/demandware.static/-/Sites-monsoon-master-catalog/default/dw23cff41c/images/large/23_64170109_2.jpg?sw=663&sh=848&sm=cut',
         altText: 'DI DOUBLE BOW JUMPER RED',
         price: 70.00,
         variants: {
@@ -37,8 +37,6 @@ const products: Product[] = [
         brand: 'Monsoon',
         description: `
         Christmas jumper day? We've got you. Cut in a classic crew neck sweater, this jumper is designed with a ribbed finish and cuff sleeves. The front is sparkling with sequin bow appliques for a more subtle take on the Christmas jumper.
-
-As part of the journey to make our clothes kinder to the world around us, this item was made using more sustainable raw materials, such as organic cotton, recycled polyester and viscose from responsibly grown forests.
 `,
     },
     {
@@ -59,8 +57,6 @@ As part of the journey to make our clothes kinder to the world around us, this i
         },
         description: `
         Nothing beats vintage. Inspired by the 50s, we've put our Monsoon twist on this dress, featuring a floral lace collar and beige spotty backdrop. The long sleeves and midi length make it suitable for styling through spring as much as winter (with the help of some tights and boots). Finished with pearlescent buttons through the bodice.
-
-        As part of the journey to make our clothes kinder to the world around us, this item was made using more sustainable raw materials, such as organic cotton, recycled polyester and viscose from responsibly grown forests.
 `,
     },
     {
@@ -144,109 +140,23 @@ As part of the journey to make our clothes kinder to the world around us, this i
     `,
     },
     {
-        id: '21045001',
-        label: '2976 LEONORE FAUX FUR LINED BURNISHED CHELSEA BOOTS',
+        id: '64634554',
+        label: 'ARDEN VELVET MAXI DRESS BLUE',
         imageUrl:
-            'https://cdn.media.amplience.net/i/drmartens/21045001.81?$smart1024$&fmt=auto',
+            'https://www.monsoon.co.uk/dw/image/v2/BDLV_PRD/on/demandware.static/-/Sites-monsoon-master-catalog/default/dwad259635/images/large/51_64634554_6.jpg?sw=663&sh=848&sm=cut',
         imageUrl2:
-            'https://cdn.media.amplience.net/i/drmartens/21045001.80?$smart1024$&fmt=auto',
-        altText: '2976 LEONORE FAUX FUR LINED BURNISHED CHELSEA BOOTS',
-        price: 179.00,
+            'https://www.monsoon.co.uk/dw/image/v2/BDLV_PRD/on/demandware.static/-/Sites-monsoon-master-catalog/default/dw5d82e8cf/images/large/23_64634554_2.jpg?sw=663&sh=848&sm=cut',
+        altText: 'ARDEN VELVET MAXI DRESS BLUE',
+        price: 125.00,
         variants: {
-            color: ['Black — BURNISHED WYOMING'],
-            size: ['3', '4', '5', '6', '7', '8', '9', '10'],
-            style: ['CHELSEA BOOTS'],
-            silhouette: ['2976 BOOTS'],
+            color: ['Blue'],
+            size: ['8', '10', '12', '14', '16', '18', '20', '22', '24'],
+            style: ['EVENING DRESSES'],
         },
         category: 'Women',
-        brand: "Dr Martens",
+        brand: "Monsoon",
         description: `
-        The 2976 Leonore is a Chelsea boot combining tomboy style with a slender, feminine silhouette for an uncompromisingly fashion-forward look and feel. Then, we winterized it, lining it in soft, artificial white fur to keep you warm in cold weather conditions.
-
-* Lined with soft faux fur
-* Chelsea boot style
-* Built on the iconic and comfortable Dr. Martens air-cushioned sole
-* Built to last with a Goodyear-welted construction
-* Made with Orleans, a lightly-textured, soft and waxy leather
-* Made with classic Doc's DNA, including grooved edges, visible stitching and heel-loop
-* The insoles in these boots are designed to be removable so don't worry if they slip out
-`,
-    },
-    {
-        id: '30981436',
-        label: 'JETTA HI MAX METALLIC LEATHER PLATFORM BOOTS',
-        imageUrl:
-            'https://cdn.media.amplience.net/i/drmartens/30981436.81?$smart1024$&fmt=auto',
-        imageUrl2:
-            'https://cdn.media.amplience.net/i/drmartens/30981436.80?$smart1024$&fmt=auto',
-        altText: 'JETTA HI MAX METALLIC LEATHER PLATFORM BOOTS',
-        price: 239.00,
-        variants: {
-            color: ['DEEP BLUE — PULL UP SHIFT'],
-            size: ['3', '4', '5', '6', '7', '8', '9', '10'],
-            style: ['CHELSEA BOOTS'],
-            silhouette: ['PLATFORM BOOTS'],
-        },
-        category: 'Women',
-        brand: 'Dr Martens',
-        description: `
-        The Jetta boot combines our classic Chelsea boot and our statement Sinclair boot. This extra-high version extends up the leg and stands on our tallest-ever Quad Max sole. Fitted with shiny silver front-facing and inner zips with rivet detailing. The Jetta Hi Max boot is built from deep blue Pull Up Shift leather, tagged with our black and yellow heel loop, and marked with our signature yellow welt stitching.
-
-* Pull Up Shift is a full grain leather with an iridescent, metallic finish
-* Our tallest sole to date — only for the brave. With a chunky commando tread pattern. A sole to make you feel on top of the world
-* Our Goodyear-welted lines are heat-sealed and reinforced with our signature welt stitch
-`,
-    },
-    {
-        id: '27999001',
-        label: '1461 UNDERCOVER SMOOTH LEATHER SHOES',
-        imageUrl:
-            'https://cdn.media.amplience.net/i/drmartens/27999001.81?$smart1024$&fmt=auto',
-        imageUrl2:
-            'https://cdn.media.amplience.net/i/drmartens/27999001.80?$smart1024$&fmt=auto',
-        altText: '1461 UNDERCOVER SMOOTH LEATHER SHOES',
-        price: 219.00,
-        category: 'Men',
-        brand: 'Dr Martens',
-        variants: {
-            color: ['Black — SMOOTH', 'Blue — SMOOTH', 'Cherry Red — SMOOTH'],
-            size: ['3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
-            style: ['OXFORD SHOES'],
-            silhouette: ['1461 SHOES'],
-        },
-        description: `
-        One of our longest-running design partners, Jun Takahashi, returns for a celebration of film and fine craftsmanship. Paying tribute to the Japanese designer’s love of 70s cinema, these 1461 shoes are printed with a distinctive grid check pattern from the Undercover archive and embossed with WE MAKE NOISE NOT CLOTHES, the label’s signature sign-off. Handcrafted in our original Wollaston factory from DM's Smooth leather. Punched with our unmistakable yellow welt stitching.  
-
-* Made in England at our original Wollaston factory 
-* Second off the DM’s production line, the 1461 is a 3-eye shoe championed for its versatility 
-* Hardwearing and famously tough, our Smooth leather can be polished to shine or scuffed-up. It’s up to you. Clean with a damp cloth and care with Dr. Martens Shoe Polish 
-* A pillar of DM’s construction. Our Original and most recognisable sole. A height of 1.25" — marked by traditional DMS tread pattern that's unique to Dr. Martens. One of our most flexible soles, well known for its comfort and stability. (3.17cm)   
-* Our Goodyear-welted lines are heat-sealed at 700°C and reinforced with our signature welt stitch 
-`,
-    },
-    {
-        id: '11857001',
-        label: "1490 SMOOTH LEATHER HIGH LACE UP BOOTS",
-        imageUrl:
-            'https://cdn.media.amplience.net/i/drmartens/11857001.81?$smart1024$&fmt=auto',
-        imageUrl2:
-            'https://cdn.media.amplience.net/i/drmartens/11857001.80?$smart1024$&fmt=auto',
-        altText: "1490 SMOOTH LEATHER HIGH LACE UP BOOTS",
-        category: 'Men',
-        brand: 'Dr Martens',
-        price: 179.00,
-        variants: {
-            color: ['Black — SMOOTH'],
-            size: ['3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
-            style: ['LACE UP BOOTS'],
-            silhouette: ['1490 BOOTS'],
-        },
-        description: `
-        The 1490 10-eye boot is a classic Dr. Martens punk silhouette — laced even higher for an amped-up attitude.
-
-* 10-eye boot
-* Made with classic Doc's DNA, including grooved edges, yellow stitching and heel-loop
-* Built on the iconic Dr. Martens air-cushioned sole, which is oil and fat resistant, with good abrasion and slip resistance
+        Bagged: A new dress and endless compliments. This piece is designed in velvet with a blend of prints in bursting hues. As flamboyant as it is feminine, it features puff shoulders and a v-neckline, with subtle definition under the bust. Finished with buttons through the front.
 `,
     },
 ];
