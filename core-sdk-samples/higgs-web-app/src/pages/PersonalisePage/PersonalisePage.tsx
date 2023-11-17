978e5b831b052f748d130dd2fbf7ad5c2ba2f56a
rpickettmp <87021361+rpickettmp@users.noreply.github.com>
2023-11-17 11:56:39 +0000

    Update PersonalisePage.tsx

--- core-sdk-samples/higgs-web-app/src/pages/PersonalisePage/PersonalisePage.tsx
+++ core-sdk-samples/higgs-web-app/src/pages/PersonalisePage/PersonalisePage.tsx
@@ -110,7 +110,7 @@ const PersonalisePage: React.FC<PersonaliseModalProps> = ({
     useEffect(() => {
         if (currUser && isOpen) { 
             fetch(
-                `https://bmpwfkut5z6iwjd44ccxqegasu0dwcsn.lambda-url.us-east-1.on.aws/profile?wSID=1042&mPID=${currUser.getMPID()}`,
+                `https://xqvmjmf4bsiktuxjtc6zlcfxfm0zapit.lambda-url.us-east-1.on.aws/profile?wSID=1042&mPID=${currUser.getMPID()}`,
                 // `https://demo.mp.com/mp/profile?wSID=432&mPID=${currUser.getMPID()}`,
             )
                 .then((response) => response.json())
